ca1bebfe5f00578bf05d234041cded85b779f941
alinekachel <83847813+alinekachel@users.noreply.github.com>
2021-10-11 10:02:34 +0900

    Minor textual correction and warning corrections

--- src/Credits.js
+++ src/Credits.js
@@ -3,14 +3,18 @@ import React from "react";
 export default function Credits() {
   return (
     <div className="credits">
-      App coded by
+      App coded by 
       <a
-        href="https://github.com/alinekachel/scplus-final-project"
+        href="https://github.com/alinekachel/weather-app-react"
         target="_blank"
+        rel="noreferrer"
       >
         Aline Kachel
       </a>
-      and hosted on <a href="https://app.netlify.com/">Netlify</a>
+       and hosted on{" "}
+      <a href="https://app.netlify.com/" target="_blank" rel="noreferrer">
+        Netlify
+      </a>
     </div>
   );
 }
